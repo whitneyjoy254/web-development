eca3353c3e030064a48d081e9ac6952cd796746a
whitneyjoy254 <146476967+whitneyjoy254@users.noreply.github.com>
2023-10-06 16:43:06 +0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,6 @@
+<?php
+//echo "rennish";
+echo $_POST["uname"]."<br/>";//.<br> this means you are joining two words
+echo $_POST["pwd"];// the word echo is for displaying the output
+////attribute class is used in css where we use
+?> 
